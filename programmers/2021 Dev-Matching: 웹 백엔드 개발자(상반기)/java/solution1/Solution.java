6184218006a606de77ada755b4947de87ab4ec9c
박민우 <minwpark@ebay.com>
2021-08-08 15:43:47 +0900

    feat: Add a problem

--- programmers/2021 Dev-Matching: 웹 백엔드 개발자(상반기)/java/solution1/Solution.java
+++ programmers/2021 Dev-Matching: 웹 백엔드 개발자(상반기)/java/solution1/Solution.java
@@ -1,4 +1,4 @@
-/*
+package solution1;/*
     Problem Name    : 로또의 최고 순위와 최저 순위
     Problem Status  : Solved
     Problem From    : 연습문제
@@ -8,16 +8,16 @@
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-class Solution1 {
+class Solution {
 
   public static void main(String[] args) {
-    Solution1.solution(new int[]{44, 1, 0, 0, 31, 25}, new int[]{31, 10, 45, 1, 6, 19});
+    Solution.solution(new int[]{44, 1, 0, 0, 31, 25}, new int[]{31, 10, 45, 1, 6, 19});
   }
 
   public static int[] solution(int[] lottos, int[] win_nums) {
     var answer =  new int[]{6, 6};
     var a = Arrays.stream(lottos).sequential().filter(s -> s == 0).count();
-    var c = Arrays.stream(lottos).sequential().filter(s -> Arrays.stream( win_nums ).boxed().collect( Collectors.toList()).contains(s)).count();
+    var c = Arrays.stream(lottos).sequential().filter(s -> Arrays.stream(win_nums).boxed().collect(Collectors.toList()).contains(s)).count();
     var e = c + a;
 
     if (e > 1)
